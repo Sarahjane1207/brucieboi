0477bf5aa76c3e161f2ba29ff8848edb7dff4982
Sarahjane1207 <150407274+Sarahjane1207@users.noreply.github.com>
2023-11-11 00:09:33 +0800

    Add files via upload

---
+++ bruce/src/main/java/com/mycompany/bruce/Bruce.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bruce;
+
+/**
+ *
+ * @author USER
+ */
+public class Bruce {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
